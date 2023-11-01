afe4e149c73602053d45afab35396b0dd4374d8e
WillsterJohnson <willster@willsterjohnson.com>
2023-11-01 21:59:11 +0000

    :sparkles: Implement time.duration DSL

--- packages/time.duration/README.md
+++ packages/time.duration/README.md
@@ -1,12 +1,83 @@
 # @wjminis/time.duration
 
+A micro-DSL for time durations in JavaScript.
+
 ## Install
+
+It only takes `` duration`1 moment` ``\*!
 
 ```sh
 pnpm add @wjminis/time.duration
 ```
 
-<!-- content here -->
+<sup>\* `moment` is not a valid <a href="#supported-units">unit specifier</a></sup>
+
+## Usage
+
+Simply type what you want in plain and simple English.
+
+```js
+import { duration } from "@wjminis/time.duration";
+
+// 3_600_000
+duration`1hr`;
+
+//  5_400_000
+duration`1hr 30min`;
+
+// 5_400_000
+duration`1hr and 30mins`;
+
+// 31_795_200_017
+duration`1yr, 3d, 17ms`;
+
+// 16.66...
+duration`${1 / 60}s`;
+
+// 1
+duration`${1}f`;
+```
+
+### Syntax
+
+The syntax for time.duration is incredibly simple. Either interpolate a number,
+or write a number in the template string, followed by a
+[unit specifier](#supported-units). You may insert `and`, `plus`, `&`, `+`, and
+`,` anywhere you like. Spaces are optional between numbers and specifiers, but
+are required between specifiers and `and` or `plus`.
+
+```js
+// these are all identical
+duration`1y7M2w4d19h47m16s2f`;
+duration`1 y 7 M 2 w 4 d 19 h 47 m 16 s 2 f`;
+duration`1 year, 7 months, 2 weeks, 4 days, 19 hours, 47 minutes, 16 seconds, and 2 milliseconds`;
+duration`1y7M2w4d19h47m16s2f and and and and and and and`; // `and` - it's pretty much a comment
+```
+
+### Supported Units
+
+For the greatest freedom of expression, time.duration supports 9 time units with 74 aliases.
+
+- Milliseconds - `f`
+  - `frac` `fracs` `fraction` `fractions` `ms` `mil` `mils` `milli` `millis` `millisecond` `milliseconds`
+  - The four aliases with double `l` can also be entered with only one `l`. eg: `milis`, `milisecond`
+- Seconds - `s`
+  - `sec` `secs` `second` `seconds`
+- Minutes - `m`
+  - `min` `mins` `minute` `minutes`
+- Hours - `h`
+  - `hr` `hrs` `hour` `hours`
+- Days - `d`
+  - `day` `days`
+- Weeks - `w`
+  - `wk` `wks` `week` `weeks`
+- Months (30 days) - `M`
+  - `mo` `mos` `month` `months`
+- Years (365 days) - `y`
+  - `yr` `yrs` `year` `years`
+- Leap Years Accounted (365.2425 days) - `Y`
+  - any of the "Years" alternative specifiers, preceeded by either `x`, `ex`, or `exact`. Spaces between permitted.
+    eg: `xyr`, `x years`, `exyrs`, `ex years`, `exact years`, etc.
 
 ## License
 
--- packages/time.duration/src/index.ts
+++ packages/time.duration/src/index.ts
@@ -1,1 +1,190 @@
-export {};
+const baseUnits = {
+  f: (qty: number) => qty * 1,
+  s: (qty: number) => baseUnits.f(qty) * 1000,
+  m: (qty: number) => baseUnits.s(qty) * 60,
+  h: (qty: number) => baseUnits.m(qty) * 60,
+  d: (qty: number) => baseUnits.h(qty) * 24,
+  w: (qty: number) => baseUnits.d(qty) * 7,
+  M: (qty: number) => baseUnits.d(qty) * 30,
+  y: (qty: number) => baseUnits.d(qty) * 365,
+  Y: (qty: number) => baseUnits.d(qty) * 365.2425,
+};
+
+const aliases = {
+  // fractions
+  frac: baseUnits.f,
+  fracs: baseUnits.f,
+  fraction: baseUnits.f,
+  fractions: baseUnits.f,
+  ms: baseUnits.f,
+  mil: baseUnits.f,
+  mils: baseUnits.f,
+  mili: baseUnits.f,
+  milis: baseUnits.f,
+  milli: baseUnits.f,
+  millis: baseUnits.f,
+  milisecond: baseUnits.f,
+  miliseconds: baseUnits.f,
+  millisecond: baseUnits.f,
+  milliseconds: baseUnits.f,
+  // seconds
+  sec: baseUnits.s,
+  secs: baseUnits.s,
+  second: baseUnits.s,
+  seconds: baseUnits.s,
+  // minutes
+  min: baseUnits.m,
+  mins: baseUnits.m,
+  minute: baseUnits.m,
+  minutes: baseUnits.m,
+  // hours
+  hr: baseUnits.h,
+  hrs: baseUnits.h,
+  hour: baseUnits.h,
+  hours: baseUnits.h,
+  // days
+  day: baseUnits.d,
+  days: baseUnits.d,
+  // weeks
+  wk: baseUnits.w,
+  wks: baseUnits.w,
+  week: baseUnits.w,
+  weeks: baseUnits.w,
+  // months
+  mo: baseUnits.M,
+  mos: baseUnits.M,
+  month: baseUnits.M,
+  months: baseUnits.M,
+  // years
+  yr: baseUnits.y,
+  yrs: baseUnits.y,
+  year: baseUnits.y,
+  years: baseUnits.y,
+  // exact years
+  ...Object.fromEntries(
+    ["yr", "yrs", "year", "years"]
+      .flatMap((key) => [
+        `x${key}`,
+        `x ${key}`,
+        `ex${key}`,
+        `ex ${key}`,
+        `exact${key}`,
+        `exact ${key}`,
+      ])
+      .map((key) => [key, baseUnits.Y]),
+  ),
+};
+
+type Unit = keyof typeof baseUnits | keyof typeof aliases;
+
+const units = ([...Object.keys(baseUnits), ...Object.keys(aliases)] as Unit[]).sort(
+  (a, b) => b.length - a.length,
+);
+
+const numberLit = /^[\d_]+\.?[\d_]*$/g;
+
+function tokenize(content: string) {
+  const tokens: (Unit | number)[] = [];
+
+  const chars = content.split("");
+  mainloop: while (chars.length) {
+    let idx = -1;
+    while (
+      chars
+        .slice(0, idx + 2)
+        .join("")
+        .match(numberLit)
+    )
+      idx++;
+    if (idx >= 0) {
+      const num = chars.splice(0, idx + 1).join("");
+      tokens.push(Number(num.replace("_", "")));
+      continue;
+    }
+
+    if (chars[0] === "+" || chars[0] === "&" || chars[0] === ",") {
+      chars.shift();
+      continue;
+    }
+    if (chars.slice(0, 3).join("") === "and") {
+      chars.splice(0, 3);
+      continue;
+    }
+    if (chars.slice(0, 4).join("") === "plus") {
+      chars.splice(0, 4);
+      continue;
+    }
+
+    for (const keyword of units) {
+      if (chars.slice(0, keyword.length).join("") === keyword) {
+        if (chars[keyword.length]?.match(/[a-zA-Z]/)) {
+          let length = keyword.length;
+          while (chars[length]?.match(/[a-zA-Z]/)) length++;
+          const token = chars.splice(0, length).join("");
+          throw new Error(`Invalid token \`${token}\``);
+        }
+        tokens.push(keyword);
+        chars.splice(0, keyword.length);
+        continue mainloop;
+      }
+    }
+
+    chars.shift();
+  }
+  return tokens;
+}
+
+function parse(content: (string | number)[]) {
+  const result: (Unit | number)[] = [];
+  for (const item of content) {
+    if (typeof item === "number") result.push(item);
+    else if (typeof item === "string") result.push(...tokenize(item));
+  }
+  return result;
+}
+
+function repr(segments: string[], ...substitutions: number[]) {
+  const source = ["duration`"];
+  for (let i = 0; i < segments.length; i++) {
+    source.push(segments[i]);
+    if (substitutions[i]) source.push(`\${${substitutions[i]}}`);
+  }
+  source.push("`");
+  return source.join("");
+}
+
+export function duration(segments: TemplateStringsArray, ...substitutions: number[]) {
+  try {
+    const params = parse(
+      substitutions.reduce((acc, sub, i) => [...acc, sub, segments[i + 1]], [segments[0]] as (
+        | string
+        | number
+      )[]) || [],
+    );
+
+    let total = 0;
+
+    for (let i = 0; i < params.length; ) {
+      const expectNumber = params[i++];
+      if (typeof expectNumber !== "number")
+        throw new Error(`Expected number, got \`${expectNumber}\``);
+
+      const expectUnit = params[i++];
+      if (!units.includes(expectUnit as Unit))
+        throw new Error(`Expected unit, got \`${expectUnit}\``);
+
+      const unitFn =
+        aliases[expectUnit as keyof typeof aliases] ??
+        baseUnits[expectUnit as keyof typeof baseUnits];
+
+      total += unitFn(expectNumber);
+    }
+
+    return total;
+  } catch (err) {
+    const source = repr([...segments], ...substitutions);
+    if (!(err instanceof Error)) throw new Error(`time.duration: ${err}\n${source}`);
+    err.message = `time.duration: ParserError: ${err.message}\n${source}`;
+    throw err;
+  }
+}
